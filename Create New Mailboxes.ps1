9f346dd67866aa23b128ddde588aeaa5b0a4bf94
mikiorla <mikiorla@gmail.com>
2015-09-04 13:05:51 +0200

    Added param

--- Create New Mailboxes.ps1
+++ Create New Mailboxes.ps1
@@ -1,13 +1,29 @@
 ﻿
+#create AD users/mailboxes from CSV
+$users = Import-Csv 'C:\Users\Milan.KTEHNIKA\Documents\trcpro-users list 3.txt' -Encoding Unicode
+#$password = 'Trcprons1' 
+$password = ConvertTo-SecureString "Trcprons1" -AsPlainText -Force
 
-$users = Import-Csv 'C:\Users\Milan.KTEHNIKA\Documents\TRCPRO users.txt' -Encoding Unicode
-$password = 'Trcprons1' 
 foreach ($user in $users)
 {
 
-$name = $user.Name
-$email = $user.'E-Mail Address'
+$param = @{
 
+OrganizationalUnit = 'ktehnika.local/TRCPRO'
+Name =  $user.Name
+Alias = $user.'Exchange Alias' 
+UserPrincipalName =$user.'User Logon Name'
+SamAccountName = $user.'User Logon Name'.Split('@')[0]
+FirstName = $user.'First Name'
+LastName = $user.'Last Name'
+Password = $password
+ResetPasswordOnNextLogon = $false
 
-New-Mailbox -Password $password -
+Office = $user.'Office'
+Department = $user.'Department'
+
+Database ='E1'
+}
+#New-Mailbox @param 
+
 }
